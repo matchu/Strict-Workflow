66f329f0963cded37a656b8616da3839a5993394
Matchu <matchu1993@gmail.com>
2011-11-04 21:08:04 -0500

    pretty blocker screen

--- content_scripts/block.js
+++ content_scripts/block.js
@@ -4,23 +4,28 @@
       var overlay = document.createElement('div'), lines = [
         'Page blocked until a Pomodoro break session starts.',
         'Back to work!'
-      ], p;
+      ], p, img = document.createElement('img');
       overlay.id = 'matchu-pomodoro-extension-overlay';
       overlay.style.position = 'fixed';
       overlay.style.left = 0;
       overlay.style.top = 0;
       overlay.style.width = '100%';
       overlay.style.height = '100%';
-      overlay.style.background = '#ddd';
       overlay.style.zIndex = 9000001;
-      overlay.style.padding = '1em';
+      overlay.style.backgroundImage = '-webkit-linear-gradient(bottom, #ccc 0%, #fff 75%)';
+      overlay.style.padding = '5em 1em 1em';
       overlay.style.textAlign = 'center';
       overlay.style.color = '#000';
-      overlay.style.font = 'normal normal normal 14px/1 sans-serif';
+      overlay.style.font = 'normal normal normal 16px/1 sans-serif';
+      
+      img.src = chrome.extension.getURL('icons/work_full.png');
+      img.style.marginBottom = '1em';
+      overlay.appendChild(img);
+      
       for(var i in lines) {
         p = document.createElement('p');
         p.innerText = lines[i];
-        p.style.margin = '0 0 1em 0';
+        p.style.margin = '0 0 .5em 0';
         overlay.appendChild(p);
       }
       document.body.appendChild(overlay);
