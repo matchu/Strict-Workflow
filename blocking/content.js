5c698d303590d5352ecc2dd97b72324c1e0541f9
Matchu <matchu1993@gmail.com>
2013-07-14 15:41:02 -0700

    use constant naming conventions for content script constants

--- blocking/content.js
+++ blocking/content.js
@@ -1,12 +1,12 @@
-var extensionId = chrome.i18n.getMessage("@@extension_id");
-var blockedClassName = extensionId + "-blocked";
-var overlayId = extensionId + "-overlay";
+var EXTENSION_ID = chrome.i18n.getMessage("@@extension_id");
+var BLOCKED_CLASS_NAME = EXTENSION_ID + "-blocked";
+var OVERLAY_ID = EXTENSION_ID + "-overlay";
 
 var blocked = false;
 
 function buildOverlay() {
   var overlay = document.createElement("div");
-  overlay.id = overlayId;
+  overlay.id = OVERLAY_ID;
   var messageKeys = ["site_blocked_info", "site_blocked_motivator"];
   messageKeys.forEach(function(key) {
     var p = document.createElement("p");
@@ -19,15 +19,15 @@ function buildOverlay() {
 function block() {
   console.log("Blocked.");
   blocked = true;
-  document.documentElement.classList.add(blockedClassName);
+  document.documentElement.classList.add(BLOCKED_CLASS_NAME);
   document.body.appendChild(buildOverlay());
 }
 
 function unblock() {
   console.log("Unblocked.");
   blocked = false;
-  document.documentElement.classList.remove(blockedClassName);
-  document.body.removeChild(document.getElementById(overlayId));
+  document.documentElement.classList.remove(BLOCKED_CLASS_NAME);
+  document.body.removeChild(document.getElementById(OVERLAY_ID));
 }
 
 function toggleBlocked(phase) {
