5eb04879eae8bda2ed74552cc9baecf5972e25b6
Matchu <matchu1993@gmail.com>
2013-07-14 21:00:48 -0700

    document onAlarm handler in event page

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,6 @@
+// We only want one "process" to be in charge of handling phase completion, so
+// don't put this in phases.js, since it gets included everywhere. Instead,
+// it's explicitly only in our event page to avoid duplicate calls.
 chrome.alarms.onAlarm.addListener(function(alarm) {
   if (alarm.name === "phaseComplete") {
     Phases.getCurrent(function(phase) {
