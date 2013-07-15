b8a32c33c40d2153c5450f0c20f280f8ce153a99
Matchu <matchu1993@gmail.com>
2013-07-14 21:46:54 -0700

    show/hide buttons appropriate and i18n

--- blocking/content.js
+++ blocking/content.js
@@ -1,12 +1,11 @@
 var EXTENSION_ID = chrome.i18n.getMessage("@@extension_id");
 var BLOCKED_CLASS_NAME = EXTENSION_ID + "-blocked";
-var OVERLAY_ID = EXTENSION_ID + "-overlay";
 
 var blocked = false;
+var overlay, controls;
 
 function buildControl(action) {
   var button = document.createElement("button");
-  button.innerText = action; // TODO: i18n, update with phases
   button.addEventListener("click", function() {
     // Phases.trigger requires API permissions that we don't have, so ask the
     // router to do it for us.
@@ -15,24 +14,33 @@ function buildControl(action) {
   return button;
 }
 
-function buildControls() {
+function buildControls(phase) {
   var wrapper = document.createElement("div");
-  ["next", "exit"].map(buildControl).forEach(function(control) {
-    wrapper.appendChild(control);
-  });
+
+  if (phase.on.next) {
+    var nextButton = buildControl("next");
+    nextButton.innerText = chrome.i18n.getMessage("start_next_" + phase.on.next);
+    wrapper.appendChild(nextButton);
+  }
+
+  if (phase.on.exit) {
+    var exitButton = buildControl("exit");
+    exitButton.innerText = chrome.i18n.getMessage("exit");
+    wrapper.appendChild(exitButton);
+  }
+
   return wrapper;
 }
 
 function buildOverlay() {
   var overlay = document.createElement("div");
-  overlay.id = OVERLAY_ID;
+  overlay.id = EXTENSION_ID + "-overlay";
   var messageKeys = ["site_blocked_info", "site_blocked_motivator"];
   messageKeys.forEach(function(key) {
     var p = document.createElement("p");
     p.innerText = chrome.i18n.getMessage(key);
     overlay.appendChild(p);
   });
-  overlay.appendChild(buildControls());
   return overlay;
 }
 
@@ -40,14 +48,26 @@ function block() {
   console.log("Blocked.");
   blocked = true;
   document.documentElement.classList.add(BLOCKED_CLASS_NAME);
-  document.body.appendChild(buildOverlay());
+  if (!overlay) {
+    overlay = buildOverlay();
+  }
+  document.body.appendChild(overlay);
 }
 
 function unblock() {
   console.log("Unblocked.");
   blocked = false;
   document.documentElement.classList.remove(BLOCKED_CLASS_NAME);
-  document.body.removeChild(document.getElementById(OVERLAY_ID));
+  document.body.removeChild(overlay);
+}
+
+function updateOverlay(phase) {
+  if (blocked) {
+    console.log("OMG we're blocked! Update overlay!", phase, phase.on.next);
+    if (controls) overlay.removeChild(controls);
+    controls = buildControls(phase);
+    overlay.appendChild(controls);
+  }
 }
 
 function toggleBlocked(phase) {
@@ -62,7 +82,10 @@ function toggleBlocked(phase) {
           unblock();
         }
       }
+      updateOverlay(phase);
     });
+  } else {
+    updateOverlay(phase);
   }
 }
 
