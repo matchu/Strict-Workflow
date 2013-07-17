1577613913964273d27195bcdbda2b58f05b9dd7
Matchu <matchu1993@gmail.com>
2013-07-16 18:29:36 -0700

    play audio on timer complete

--- notifications/events.js
+++ notifications/events.js
@@ -1,12 +1,16 @@
 Phases.onChanged.addListener(function(phaseName) {
   var phase = Phases.get(phaseName);
   if (phase.notification) {
-    Options.get("notifications", function(items) {
+    Options.get(["notifications", "audio"], function(items) {
       if (items.notifications) {
         // TODO: support rich notifications for compatible platforms
         var notification = webkitNotifications.
           createHTMLNotification("/notifications/notification.html");
         notification.show();
+      }
+      if (items.audio) {
+        var ring = new Audio("/ring.ogg");
+        ring.play();
       }
     });
   }
